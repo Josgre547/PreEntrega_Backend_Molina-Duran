b6189f742fe5fd7b8f41b7af6df93103944ef409
Jose Molina <josgre547@hotmail.com>
2024-06-03 23:54:08 -0300

    cambios menores

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import __dirname from "./dirname.js";
 import handlebars from "express-handlebars";
 import { Server } from "socket.io";
 import viewsRoutes from "./router/views.routes.js";
+import productManager from "./productManager.js";
 
 const app = express();
 
@@ -24,6 +25,8 @@ const httpServer = app.listen(8080, () => {
 
 export const io = new Server(httpServer);
 
-io.on("connection", (socket) => {
+io.on("connection", async socket => {
   console.log("Nuevo usuario Conectado");
+  const products = await productManager.getProducts();
+  io.emit('products', {products});
 });
--- src/router/views.routes.js
+++ src/router/views.routes.js
@@ -16,9 +16,6 @@ router.get("/", async (req, res) => {
 
 router.get("/realtimeproducts", async (req, res) => {
   try {
-    const products = await productManager.getProducts();
-    io.emit("products", products);
-
     res.render("realTimeProducts");
   } catch (error) {
     console.log(error);
